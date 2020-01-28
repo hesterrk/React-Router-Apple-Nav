439544835ae46e54d770a07f6c092e929457eb17
Hester Knight <hesterknight@hesters-mbp.mynet>
2020-01-28 14:14:50 +0000

    added some things to wrapper

--- apple-nav/src/components/NavWrapper.jsx
+++ apple-nav/src/components/NavWrapper.jsx
@@ -5,8 +5,21 @@ import React from 'react';
 function NavWrapper() {
 
     return (
-<div>
+<div className="navwrapperContainer">
         <p>What Product Would You Like to Look at Today?</p>
+
+<div className="unorderedList">
+       
+        <ul>
+        <li> Mac </li>
+        <li> Ipad </li>
+        <li> Iphone </li>
+        <li> Watch </li>
+        <li> TV </li>
+        <li> Music </li>
+        </ul>
+        </div>
+
 
 </div>
 
